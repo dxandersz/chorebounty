7c7cf94b2ae25f4385039ac27c965bee30a34a95
dxandersz <d.c.anderson@live.com>
2021-07-08 16:36:54 -0700

    Additional middleware for auth0 added.

--- server/server.js
+++ server/server.js
@@ -1,12 +1,23 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+
 const app = express();
 const db = require("./db");
 
 // MIDDLEWARE! //
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(morgan("combined"));
+app.use(helmet());
+app.use(cors());
+
+
 
 // ROUTES!! //
 
