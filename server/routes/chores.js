d0ab84c2662817afa9886575fa78c8cb4ea215d5
dxandersz <d.c.anderson@live.com>
2021-07-31 20:01:07 -0700

    JWT authentication integrated, basic login and registration functioning

--- server/routes/chores.js
+++ server/routes/chores.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const { Pool } = require("pg");
 const db = require("../db");
 
 // GET ALL CHORES //
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const { Pool } = require("pg");
 const db = require("../db");
+const bcrypt = require("bcrypt");
+const jwtGenerator = require("../utils/jwtGenerator");
 
-// GET ALL USERS
+// GET ALL USERS ROUTE
 router.get('/', async (req, res) => {
     console.log(req.body);
 
@@ -22,16 +24,59 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.post("/", async (req, res) => {
+// REGISTER ROUTE
+router.post("/register", async (req, res) => {
+    // Verify that email and username aren't already in use.
     try {
         const { name, email, password } = req.body;
-        const user = await db.query("SELECT * FROM users WHERE user_email = $1", 
-        [email]);
+        const user = await db.query("SELECT * FROM users WHERE user_email = $1 OR user_name = $2", [email, name]);
+        if (user.rows.length !== 0) {
+            return res.status(401).send("Email or username already taken.");
+        };
 
-        res.json(user.rows);
+        // Use Bcrypt to conceal the user password.
+        const saltRound = 10;
+        const salt = await bcrypt.genSalt(saltRound);
+
+        const bcryptPassword = await bcrypt.hash(password, salt)
+
+        // Register the user in the database
+        const newUser = await db.query("INSERT INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *", [name, email, bcryptPassword]);        
+        // Create a JWT token
+        const token = jwtGenerator(newUser.rows[0].user_id);
+        res.json({ token });
+
     } catch (err) {
         console.log(err);
     }
 });
+
+// LOGIN ROUTE
+router.post('/login', async (req, res) => {
+    try {
+        // Destructure req.body
+        const { email, password } = req.body;
+        // Check for user's existence
+        const user = await db.query("SELECT * FROM users WHERE user_email = $1", [email]);
+
+        if (user.rows.length === 0) {
+            return res.status(401).send("Incorrect Email or Password Entered")
+        }
+
+        // Authenticate incoming password against database
+        const validPassword = await bcrypt.compare(password, user.rows[0].user_password);
+        
+        if (!validPassword) {
+            return res.status(401).json("Password or Email is Incorrect");
+        }
+        // Give user jwt token
+        const token = jwtGenerator(user.rows[0].user_id);
+        res.json({ token });
+        
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+})
 
 module.exports = router;
